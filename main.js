fb0cd7a06ddbc55891e3f7ec5df4ca5f67a7b37c
georgekunchattil1992 <georgekunchattil@gmail.com>
2017-03-17 13:49:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log('Loaded!');
+
+//Change the text of the main-text div
+var element=socument.getElementById('main-text');
+
+element.innerHTML="New value";
+
+/Move the image
+var img=document.getElementById('madi');
+img.onclick=function(){
+	img.style.marginLeft='100px';
+};
